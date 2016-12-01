8dc78a9ff49478775f2561c0276a8bd2a33f3220
Jorge Bernal <jbernalmartinez@gmail.com>
2016-12-01 21:45:00 +0100

    UPBGE: TrackTo Actuator moved&cleaned

--- source/gameengine/GameLogic/SCA_TrackToActuator.h
+++ source/gameengine/GameLogic/SCA_TrackToActuator.h
@@ -25,12 +25,12 @@
  * ***** END GPL LICENSE BLOCK *****
  */
 
-/** \file KX_TrackToActuator.h
- *  \ingroup ketsji
+/** \file SCA_TrackToActuator.h
+ *  \ingroup gamelogic
  */
 
-#ifndef __KX_TRACKTOACTUATOR_H__
-#define __KX_TRACKTOACTUATOR_H__
+#ifndef __SCA_TRACKTOACTUATOR_H__
+#define __SCA_TRACKTOACTUATOR_H__
 
 #include "SCA_IActuator.h"
 #include "SCA_IObject.h"
@@ -38,11 +38,11 @@
 #include "KX_GameObject.h"
 
 
-class KX_TrackToActuator : public SCA_IActuator
+class SCA_TrackToActuator : public SCA_IActuator
 {
 	Py_Header
 	// Object reference. Actually, we use the object's 'life'
-	SCA_IObject*	m_object;
+	SCA_IObject *m_object;
 	// 3d toggle
 	bool m_allow3D;
 	// time field
@@ -51,17 +51,17 @@ class KX_TrackToActuator : public SCA_IActuator
 	int m_upflag;
 	
 	MT_Matrix3x3 m_parentlocalmat;
-	KX_GameObject* m_parentobj;
+	KX_GameObject *m_parentobj;
 
   public:
-	KX_TrackToActuator(SCA_IObject* gameobj, SCA_IObject *ob, int time,
-	                   bool threedee,int trackflag,int upflag);
-	virtual ~KX_TrackToActuator();
+	SCA_TrackToActuator(SCA_IObject* gameobj, SCA_IObject *ob, int time,
+	                    bool threedee, int trackflag, int upflag);
+	virtual ~SCA_TrackToActuator();
 	virtual CValue* GetReplica() {
-		KX_TrackToActuator* replica = new KX_TrackToActuator(*this);
+		SCA_TrackToActuator* replica = new SCA_TrackToActuator(*this);
 		replica->ProcessReplica();
 		return replica;
-	};
+	}
 
 	virtual void ProcessReplica();
 	virtual bool UnlinkObject(SCA_IObject* clientobj);
@@ -93,6 +93,6 @@ class KX_TrackToActuator : public SCA_IActuator
 	
 #endif  /* WITH_PYTHON */
 
-}; /* end of class KX_TrackToActuator : public KX_EditObjectActuator */
+}; /* end of class SCA_TrackToActuator : public SCA_EditObjectActuator */
 
-#endif  /* __KX_TRACKTOACTUATOR_H__ */
+#endif  /* __SCA_TRACKTOACTUATOR_H__ */
