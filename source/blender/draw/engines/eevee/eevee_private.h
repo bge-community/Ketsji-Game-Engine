c1743f5daf3d5953cd42f1dfd20b6b2a6abd6641
Ulysse Martin <youle31@users.noreply.github.com>
2017-11-08 21:29:10 +0100

    branch to do experiments with eevee's probes
    copied eevee's probes code in new file to work on adaptation for bge

--- source/blender/draw/engines/eevee/eevee_private.h
+++ source/blender/draw/engines/eevee/eevee_private.h
@@ -655,6 +655,28 @@ static const float cubefacemat[6][4][4] = {
 };
 
 /****************Game engine********************/
+typedef struct EEVEE_LightProbeStaticData {
+	struct GPUShader *probe_default_sh;
+	struct GPUShader *probe_filter_glossy_sh;
+	struct GPUShader *probe_filter_diffuse_sh;
+	struct GPUShader *probe_grid_fill_sh;
+	struct GPUShader *probe_grid_display_sh;
+	struct GPUShader *probe_planar_display_sh;
+	struct GPUShader *probe_planar_downsample_sh;
+	struct GPUShader *probe_cube_display_sh;
+
+	struct GPUTexture *hammersley;
+	struct GPUTexture *planar_minmaxz;
+	struct GPUTexture *planar_pool_placeholder;
+	struct GPUTexture *depth_placeholder;
+	struct GPUTexture *depth_array_placeholder;
+	struct GPUTexture *cube_face_depth;
+	struct GPUTexture *cube_face_minmaxz;
+
+	int update_world;
+	bool world_ready_to_shade;
+} EEVEE_LightProbeStaticData;
+
 struct GPUShader *EEVEE_shadow_shader_get();
 struct GPUShader *EEVEE_shadow_store_shader_get();
 void EEVEE_effects_replace_e_data_depth(struct GPUTexture *depth_src);
@@ -665,6 +687,7 @@ void EEVEE_shgroup_add_standard_uniforms_game(struct DRWShadingGroup *shgrp,
 	int *ssr_id, float *refract_depth, bool use_ssrefraction);
 EEVEE_Data *EEVEE_engine_data_get(void);
 void EEVEE_draw_shadows_bge(struct EEVEE_SceneLayerData *sldata, struct EEVEE_PassList *psl);
+EEVEE_LightProbeStaticData *EEVEE_lightprobes_static_data_get();
 /**************End of Game engine***************/
 
 #endif /* __EEVEE_PRIVATE_H__ */
